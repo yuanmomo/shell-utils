8eaa4d11684c915f09e93de233de430dead225ba
aaaaa <aaaa@gmail.com>
2019-09-29 14:16:11 +0800

    update ssh

--- network/ssh-util.sh
+++ network/ssh-util.sh
@@ -9,6 +9,7 @@ cat << EOF
     1.1 修改 sshd port
     1.2 禁用密码登陆，仅允许免密登陆
     1.3 禁用 DNS 反向解析
+2. 自动安装 ufw，并开启 ssh 端口
 EOF
 
 # 读取用户输入脚本
@@ -22,8 +23,20 @@ chmod +x ${read_input_file} && source ${read_input_file}
 readInput "是否需要修改 SSH 配置, (y/n)? (默认: n) " "^([y]|[n])$" "n"
 changeSsh=${read_value}
 
-if [[ ${changeSsh} == y ]]; then
+# Debian(Ubuntu) or RHEL(CentOS)
+cmd="apt"
+if [[ $(command -v yum) ]]; then
+	cmd="yum"
+fi
 
+if [[ ! $(command -v ufw) ]]; then
+    # install ufw
+    ${cmd} install -y ufw
+    ufw enable
+    ufw default deny
+fi
+
+if [[ ${changeSsh} == y ]]; then
     readInput "请指定 SSH 新的端口号 (可用范围为0-65535), 默认 27392:  ? " "^([0-9]{1,4}|[1-5][0-9]{4}|6[0-5]{2}[0-3][0-5])$" "27392"
     Port=${read_value}
 
@@ -42,7 +55,11 @@ if [[ ${changeSsh} == y ]]; then
 
     service sshd restart
 
-    iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $Port -j ACCEPT
-    ip6tables -I INPUT -m state --state NEW -m tcp -p tcp --dport $Port -j ACCEPT
+    # iptables -I INPUT -m state --state NEW -m tcp -p tcp --dport $Port -j ACCEPT
+    # ip6tables -I INPUT -m state --state NEW -m tcp -p tcp --dport $Port -j ACCEPT
     echo "新的 SSH 端口号 : $Port"
+
+    echo "防火墙开放 SSH 端口号 : $Port"
+    ufw allow $Port/tcp
+    ufw status numbered
 fi
